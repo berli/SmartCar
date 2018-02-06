c0fbb3993307a12cd10c4b0a3f92d38bb2923bf1
TianYuan <yuan.tian@timanetworks.com>
2018-02-06 18:36:42 +0800

    pods集成 RAC

---
+++ SmartCarAPP/SmartCar/SCDataViewModel.h
@@ -0,0 +1,13 @@
+//
+//  SCDataViewModel.h
+//  SmartCar
+//
+//  Created by TianYuan on 2018/2/6.
+//  Copyright © 2018年 SmartCar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCDataViewModel : NSObject
+
+@end
---
+++ SmartCarAPP/SmartCar/SCDataViewModel.m
@@ -0,0 +1,13 @@
+//
+//  SCDataViewModel.m
+//  SmartCar
+//
+//  Created by TianYuan on 2018/2/6.
+//  Copyright © 2018年 SmartCar. All rights reserved.
+//
+
+#import "SCDataViewModel.h"
+
+@implementation SCDataViewModel
+
+@end
